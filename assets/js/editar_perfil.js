2550b1145835f9a4994186bf9608599d167d502c
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2025-10-01 18:24:02 -0300

    Mais ajustes, agora na edição de telefone

--- assets/js/editar_perfil.js
+++ assets/js/editar_perfil.js
@@ -1,11 +1,10 @@
 function updateUserDataDisplay() {
-
   const elementsToUpdate = {
     'userName': ['user-name-display', 'user-name-config', 'welcome-message-home'],
-    'userNickname': ['user-nickname-display'], 
+    'userNickname': ['user-nickname-display'],
     'matricula': ['matricula-display'],
     'birthDate': ['birthdate-display'],
-    'phone': ['phone-display'],
+    'phone': ['phone-display', 'user-phone-header'],
     'email': ['email-display'],
     'gender': ['gender-display']
   };
@@ -38,9 +37,64 @@ if (document.getElementById('edit-name-form')) {
     const newName = input.value.trim();
     if (newName) {
       localStorage.setItem('userName', newName);
-      history.back(); 
+      history.back();
     }
   });
 }
 
-document.addEventListener('DOMContentLoaded', updateUserDataDisplay);
+if (document.getElementById('edit-phone-form')) {
+  const form = document.getElementById('edit-phone-form');
+  const input = document.getElementById('phone-input');
+
+  input.value = localStorage.getItem('phone') || '+55 11 98996 1425';
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    const newPhone = input.value.trim();
+    if (newPhone) {
+      localStorage.setItem('phone', newPhone);
+      history.back();
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  updateUserDataDisplay();
+
+  const copyButton = document.getElementById('copy-button');
+  const matriculaCode = document.getElementById('matricula-display'); 
+
+  if (copyButton && matriculaCode) {
+      copyButton.addEventListener('click', function() {
+          navigator.clipboard.writeText(matriculaCode.textContent).then(() => {
+              showToast('Código de matrícula copiado!');
+          }).catch(err => {
+              console.error('Erro ao copiar texto: ', err);
+              showToast('Erro ao copiar.');
+          });
+      });
+  }
+
+  const changePhotoButton = document.getElementById('change-photo-button');
+  const fileInput = document.getElementById('file-input');
+  const avatarImage = document.getElementById('avatar-image');
+
+  if (changePhotoButton && fileInput && avatarImage) {
+      changePhotoButton.addEventListener('click', function() {
+          fileInput.click(); 
+      });
+
+      fileInput.addEventListener('change', function(event) {
+          const file = event.target.files[0];
+
+          if (file) {
+              const reader = new FileReader();
+              reader.onload = function(e) {
+                  avatarImage.src = e.target.result;
+                  showToast('Foto de perfil atualizada!');
+              }
+              reader.readAsDataURL(file);
+          }
+      });
+  }
+});
