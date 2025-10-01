ed9780b88ab27e8ddf6c429abbe26a91c546f953
Maiara Barbosa Nunes <maiaranunes.dev@gmail.com>
2025-10-01 17:53:18 -0300

    adição de paginas faltantes

---
+++ assets/js/faq.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const faqItems = document.querySelectorAll('.faq-question');
+
+  faqItems.forEach(item => {
+    item.addEventListener('click', function () {
+      this.classList.toggle('active');
+
+      const answer = this.nextElementSibling;
+      if (answer.style.maxHeight) {
+        answer.style.maxHeight = null;
+      } else {
+        answer.style.maxHeight = answer.scrollHeight + "px";
+      }
+    });
+  });
+});
